ca3c0e8f57cefc31cacfbbd509e9470db7225dde
alexdsm1998 <alexdelsaz@gmail.com>
2018-09-21 09:17:26 +0200

    actividad terminada

---
+++ actividad01.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    for ($i=0; $i < 255 ; $i++) {
+      echo "<div style='background-color:rgb(0,$i,0)'>(0,$i,0)</div>";
+    }
+    ?>
+  </body>
+</html>
